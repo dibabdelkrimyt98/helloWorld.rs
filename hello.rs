f01107e69f45d426fbbd07ab4b2d84b3d87d1f7f
dibabdelkrimyt98 <dibabdelkrimyt98@gmail.com>
2024-12-05 13:53:30 +0100

    Initial commit

---
+++ hello.rs
@@ -0,0 +1,12 @@
+fn main(){
+    // tuples
+    /* 
+    let my_data: (i8, &str, &str, bool) = (01, "DIB", "Abdelkrim Y T", true);
+    println!("Hello {:?}", my_data);
+    */
+    /* 
+    //Arrays
+    let my_array: [i64; 5] = [1, 2, 3, 4, 5]; 
+    println!("Hello My Array is : {:?}", my_array);
+    */
+}
---
+++ helloWorld.rs
@@ -0,0 +1,3 @@
+fn main (){
+    println!("Hello world, it's my first program with RUST Programming language");
+}
